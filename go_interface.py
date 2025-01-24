82fd37bed31f3691a5584a8ce67adc3d91f07cd1
NazarEE127 <nazarfedotko50@gmail.com>
2025-01-24 23:43:30 +0700

    первая нормальная версия игры
    рандомная генерация труб, есть текстуры птицы и труб, отсутствует система счёта и оптимизация

--- go_interface.py
+++ go_interface.py
@@ -1,7 +1,9 @@
 import pygame
 
+
 # Надпись "Игра окончена"
 def display_game_over(screen, width, height):
+    screen.fill((255, 255, 255))
     font = pygame.font.Font(None, 74)
     text_surface = font.render("Игра окончена", True, (255, 0, 0))
     text_rect = text_surface.get_rect(center=(width // 2, height // 2))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from go_interface import display_game_over  # Импортируем функцию из go_interface
 import random
+import os
+import sys
 
 # Инициализация Pygame
 pygame.init()
@@ -16,7 +18,6 @@ BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-
 # Настройки игры
 GRAVITY = 0.5
 bird_movement = 0
@@ -30,51 +31,21 @@ PIPE_WIDTH = 70
 PIPE_HEIGHT = 500
 FLAP_STRENGTH = -10
 
-# Загрузка изображений
-bird_surface = pygame.Surface((34, 24))
-bird_surface.fill(BLUE)
 
-pipe_surface = pygame.Surface((52, 400))
-pipe_surface.fill(GREEN)
-
-# # Создание труб
-# def create_pipe():
-#     height = random.randint(200, 400)
-#     bottom_pipe = pipe_surface.get_rect(midtop=(500, height))
-#     top_pipe = pipe_surface.get_rect(midbottom=(500, height - 150))
-#     return bottom_pipe, top_pipe
-#
-# # Движение труб
-# def move_pipes(pipes):
-#     for pipe in pipes:
-#         pipe.centerx -= 5
-#     return pipes
-#
-# # Отображение труб на экране
-# def draw_pipes(pipes):
-#     for pipe in pipes:
-#         if pipe.bottom >= HEIGHT:
-#             screen.blit(pipe_surface, pipe)
-#         else:
-#             flipped_pipe = pygame.transform.flip(pipe_surface, False, True)
-#             screen.blit(flipped_pipe, pipe)
-#
-# # Проверка столкновений
-# def check_collision(pipes):
-#     for pipe in pipes:
-#         if bird_rect.colliderect(pipe):
-#             return False
-#     if bird_rect.top <= -50 or bird_rect.bottom >= HEIGHT:
-#         return False
-#     return True
-
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
 
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((BIRD_WIDTH, BIRD_HEIGHT))
-        self.image.fill((255, 255, 0))  # Желтый цвет для птицы
+        self.image = pygame.transform.scale(load_image("bird.png"), (BIRD_WIDTH, BIRD_HEIGHT))
         self.rect = self.image.get_rect(center=(100, HEIGHT // 2))
         self.velocity = 0
 
@@ -85,16 +56,12 @@ class Bird(pygame.sprite.Sprite):
     def flap(self):
         self.velocity = FLAP_STRENGTH
 
-class Pipe(pygame.sprite.Sprite):
-    def __init__(self, x):
-        super().__init__()
-        self.image = pygame.Surface((PIPE_WIDTH, PIPE_HEIGHT))
-        self.image.fill(GREEN)
-        height = random.randint(200, 400)
-        self.rect = self.image.get_rect(midtop=(500, height))
 
-        # bottom_pipe = pipe_surface.get_rect(midtop=(500, height))
-        # top_pipe = pipe_surface.get_rect(midbottom=(500, height - 150))
+class Down_pipe(pygame.sprite.Sprite):
+    def __init__(self, h):
+        super().__init__()
+        self.image = pygame.transform.scale(load_image("pipe.png"), (PIPE_WIDTH, PIPE_HEIGHT))
+        self.rect = self.image.get_rect(midtop=(500, h))
 
     def update(self):
         self.rect.x -= 5
@@ -102,15 +69,52 @@ class Pipe(pygame.sprite.Sprite):
             self.kill()
 
 
+class Up_pipe(pygame.sprite.Sprite):
+    def __init__(self, h):
+        super().__init__()
+        self.image = pygame.transform.scale(load_image("pipe.png"), (PIPE_WIDTH, PIPE_HEIGHT))
+        self.rect = self.image.get_rect(midbottom=(500, h - 150))
+
+    def update(self):
+        self.rect.x -= 5
+        if self.rect.x < -PIPE_WIDTH:
+            self.kill()
+
+
+# Создание труб
+def create_pipe():
+    height = random.randint(100, 450)
+    bottom_pipe = Down_pipe(height)
+    top_pipe = Up_pipe(height)
+    all_sprites.add(bottom_pipe)
+    pipes.add(bottom_pipe)
+    all_sprites.add(top_pipe)
+    pipes.add(top_pipe)
+    return bottom_pipe, top_pipe
+
+
+# Отображение труб на экране
+def draw_pipes(pipes):
+    for pipe in pipes:
+        if pipe.rect.bottom >= HEIGHT:
+            pipes.draw(screen)
+        else:
+            flipped_pipe = pygame.transform.flip(screen, False, True)
+            pipes.draw(flipped_pipe)
+
+
 # Основной игровой цикл
 clock = pygame.time.Clock()
+
 all_sprites = pygame.sprite.Group()
 pipes = pygame.sprite.Group()
+
 bird = Bird()
 all_sprites.add(bird)
 
-# SPAWNPIPE = pygame.USEREVENT
-# pygame.time.set_timer(SPAWNPIPE, 1200)
+
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE, 1300)
 
 
 while True:
@@ -128,6 +132,9 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN and game_active:
             bird.flap()
 
+        if event.type == SPAWNPIPE:
+            pipes.add(create_pipe())
+
     all_sprites.update()
     screen.fill(WHITE)
     all_sprites.draw(screen)
@@ -135,15 +142,9 @@ while True:
     if game_active:
         if pygame.sprite.spritecollide(bird, pipes, False) or bird.rect.top <= -50 or bird.rect.bottom >= HEIGHT:
             game_active = False
-            print("f")
         else:
             game_active = True
-
-        # Генерация труб
-        if random.randint(1, 500) <= 10:  # Примерный шанс появления трубы
-            pipe = Pipe(WIDTH)
-            all_sprites.add(pipe)
-            pipes.add(pipe)
+        draw_pipes(pipes)
 
     else:
         # Вызов функции для отображения сообщения "Игра окончена"
@@ -151,65 +152,3 @@ while True:
 
     pygame.display.flip()
     clock.tick(60)
-
-# clock = pygame.time.Clock()
-# bird_rect = bird_surface.get_rect(center=(100, HEIGHT // 2))
-# SPAWNPIPE = pygame.USEREVENT
-# pygame.time.set_timer(SPAWNPIPE, 1200)
-# while True:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#             exit()
-#         if event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_SPACE and game_active:
-#                 bird_movement = 0
-#                 bird_movement -= 10
-#             if event.key == pygame.K_SPACE and not game_active:
-#                 game_active = True
-#                 pipes.clear()
-#                 bird_rect.center = (100, HEIGHT // 2)
-#                 bird_movement = 0
-#                 score = 0
-#             if event.key == pygame.K_UP and game_active:
-#                 bird_movement = 0
-#                 bird_movement -= 10
-#             if event.key == pygame.K_UP and not game_active:
-#                 game_active = True
-#                 pipes.clear()
-#                 bird_rect.center = (100, HEIGHT // 2)
-#                 bird_movement = 0
-#                 score = 0
-#
-#         # Обработка нажатия мыши для прыжка
-#         if event.type == pygame.MOUSEBUTTONDOWN and game_active:
-#             bird_movement = 0
-#             bird_movement -= 10  # Прыжок
-#         if event.type == pygame.MOUSEBUTTONDOWN and not game_active:
-#             game_active = True
-#             pipes.clear()
-#             bird_rect.center = (100, HEIGHT // 2)
-#             bird_movement = 0
-#             score = 0
-#
-#         if event.type == SPAWNPIPE:
-#             pipes.extend(create_pipe())
-#
-#     screen.fill(WHITE)
-#
-#     if game_active:
-#         # Движение птицы
-#         bird_movement += gravity
-#         bird_rect.centery += bird_movement
-#         game_active = check_collision(pipes)
-#
-#         # Отображение птицы и труб
-#         screen.blit(bird_surface, bird_rect)
-#         pipes = move_pipes(pipes)
-#         draw_pipes(pipes)
-#
-#     else:
-#         display_game_over(screen, WIDTH, HEIGHT)  # Вызов функции для отображения сообщения "Игра окончена"
-#
-#     pygame.display.flip()
-#     clock.tick(120)

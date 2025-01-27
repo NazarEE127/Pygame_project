b1236e83b31b3b48a95133c45f466b299611f175
LaOgree <yar.krs24@gmail.com>
2025-01-27 18:20:40 +0700

    Добавлена кнопка перезапуска игры, немного изменено отображение труб

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-#from go_interface import *  # Импортируем функцию из go_interface
 import random
 import os
 import sys
@@ -22,7 +21,6 @@ BLUE = (0, 0, 255)
 GRAVITY = 0.5
 bird_movement = 0
 
-
 # Константы
 BIRD_WIDTH = 34
 BIRD_HEIGHT = 24
@@ -31,14 +29,12 @@ PIPE_HEIGHT = 500
 FLAP_STRENGTH = -10
 
 
-def load_image(name, colorkey=None):
+def load_image(name):
     fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
-    image = pygame.image.load(fullname)
-    return image
+    return pygame.image.load(fullname)
 
 
 class Bird(pygame.sprite.Sprite):
@@ -72,7 +68,7 @@ class Down_pipe(pygame.sprite.Sprite):
 class Up_pipe(pygame.sprite.Sprite):
     def __init__(self, h):
         super().__init__()
-        self.image = pygame.transform.scale(load_image("pipe.png"), (PIPE_WIDTH, PIPE_HEIGHT))
+        self.image = pygame.transform.scale(load_image("alter_pipe.png"), (PIPE_WIDTH, PIPE_HEIGHT))
         self.rect = self.image.get_rect(midbottom=(500, h - 150))
 
     def update(self):
@@ -80,7 +76,6 @@ class Up_pipe(pygame.sprite.Sprite):
         if self.rect.x < -PIPE_WIDTH:
             self.kill()
             bird.score += 1
-
 
 
 # Создание труб
@@ -95,14 +90,10 @@ def create_pipe():
     return bottom_pipe, top_pipe
 
 
-# Отображение труб на экране
 def draw_pipes(pipes):
     for pipe in pipes:
-        if pipe.rect.bottom >= HEIGHT:
-            pipes.draw(screen)
-        else:
-            flipped_pipe = pygame.transform.flip(screen, False, True)
-            pipes.draw(flipped_pipe)
+        screen.blit(pipe.image, pipe.rect.topleft)
+
 
 def menu():
     fon = pygame.transform.scale(load_image('menu_fon.jpg'), (WIDTH, HEIGHT))
@@ -113,15 +104,17 @@ def menu():
     main_font = pygame.font.Font(None, 24)
 
     text_surface2 = main_font.render(f"Бесконечный уровень", True, pygame.Color('black'))
-    text_rect2 = text_surface.get_rect(center=(50, 100))
+    text_rect2 = text_surface2.get_rect(center=(WIDTH // 2, 100))
+
     screen.blit(text_surface, text_rect)
     screen.blit(text_surface2, text_rect2)
     button_surface = pygame.Surface((50, 50))
     button_img = pygame.transform.scale(load_image('button.png'), (50, 50))
     button_surface.blit(button_img, (0, 0))
-    button_rect = pygame.Rect(200, 70, 50, 50)
-    #pygame.draw.lines(button_surface, (255, 0, 0), True,[[10, 10], [40, 25], [10, 40]], 2)
-    screen.blit(button_surface, (button_rect.x, button_rect.y))
+
+    button_rect = button_surface.get_rect(center=(200, 70))
+
+    screen.blit(button_surface, button_rect)
 
     while True:
         for event in pygame.event.get():
@@ -130,7 +123,7 @@ def menu():
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if button_rect.collidepoint(event.pos):
-                    return # начинаем игру
+                    return
 
         pygame.display.flip()
         clock.tick(60)
@@ -138,7 +131,7 @@ def menu():
 
 def start_screen():
     intro_text = ["Flappy Bird",
-                  "  by Yandex lyceum", "",
+                  "by Yandex lyceum", "",
                   'Нажмите "Space"']
 
     fon = pygame.transform.scale(load_image('start_fon.jpg'), (WIDTH, HEIGHT))
@@ -146,7 +139,7 @@ def start_screen():
     font = pygame.font.Font(None, 30)
     text_coord = 50
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
+        string_rendered = font.render(line, True, pygame.Color('black'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
@@ -160,7 +153,8 @@ def start_screen():
                 pygame.quit()
                 exit()
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                return  # начинаем игру
+                return
+
         pygame.display.flip()
         clock.tick(60)
 
@@ -173,60 +167,64 @@ def infinit_lvl(game_active):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and game_active:
-                    bird.flap()
-                if event.key == pygame.K_UP and game_active:
+            if event.type == pygame.KEYDOWN and game_active:
+                if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
                     bird.flap()
 
-            # Обработка нажатия мыши для прыжка
             if event.type == pygame.MOUSEBUTTONDOWN and game_active:
                 bird.flap()
 
             if event.type == SPAWNPIPE:
-                two_pipe = create_pipe()
-                pipes.add(two_pipe)
+                create_pipe()
 
         all_sprites.update()
         screen.blit(fon, (0, 0))
         all_sprites.draw(screen)
         font = pygame.font.Font(None, 50)
         text_surface = font.render(f"Счёт:{bird.score}", True, (255, 0, 0))
-        text_rect = text_surface.get_rect(center=(50, 50))
+        text_rect = text_surface.get_rect(center=(60, 30))
         screen.blit(text_surface, text_rect)
 
         if game_active:
             if pygame.sprite.spritecollide(bird, pipes, False) or bird.rect.top <= -50 or bird.rect.bottom >= HEIGHT:
                 game_active = False
-            else:
-                game_active = True
-            draw_pipes(pipes)
 
+            draw_pipes(pipes)
         else:
             return
+
         pygame.display.flip()
         clock.tick(60)
 
-def display_game_over(screen, width, height):
-    screen.fill((255, 255, 255))
+
+def display_game_over(screen):
+    screen.fill(WHITE)
+
     font = pygame.font.Font(None, 74)
     text_surface = font.render("Игра окончена", True, (255, 0, 0))
-    text_rect = text_surface.get_rect(center=(width // 2, height // 2))
+
+    text_rect = text_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 50))
+
     screen.blit(text_surface, text_rect)
+
+    # Загрузка изображения перезагрузки
+    reboot_image = pygame.transform.scale(load_image('reboot.png'), (50, 50))
+
+    reboot_rect = reboot_image.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 50))
+
+    screen.blit(reboot_image, reboot_rect)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                pygame.quit()
-                exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if reboot_rect.collidepoint(event.pos):
+                    return True  # Возвращаем True для перезапуска игры
+
         pygame.display.flip()
         clock.tick(60)
-
-
 
 
 # Основной игровой цикл
@@ -239,8 +237,6 @@ bird = Bird()
 all_sprites.add(bird)
 
 game_active = True
-score = 0
-
 
 SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1300)
@@ -248,5 +244,18 @@ pygame.time.set_timer(SPAWNPIPE, 1300)
 while True:
     start_screen()
     menu()
-    infinit_lvl(game_active)
-    display_game_over(screen, WIDTH, HEIGHT)
+
+    # Игровой цикл
+    while game_active:
+        infinit_lvl(game_active)
+
+        # Проверка на завершение игры и отображение экрана завершения
+        game_active_result = display_game_over(screen)
+
+        # Если игра завершена и возвращаемся в основной цикл для перезапуска игры.
+        if game_active_result:
+            all_sprites.empty()
+            pipes.empty()
+            bird.score = 0
+            bird.__init__()
+            all_sprites.add(bird)
